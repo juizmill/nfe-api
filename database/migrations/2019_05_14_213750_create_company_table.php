ca491bff7007c289d94d0d935b5b71a681d34b3e
Jesus Vieira <jesusvieiradelima@gmail.com>
2020-02-23 18:07:12 -0300

    Migration e Model construida

--- database/migrations/2019_05_14_213750_create_company_table.php
+++ database/migrations/2019_05_14_213750_create_company_table.php
@@ -6,70 +6,40 @@ use Illuminate\Database\Migrations\Migration;
 
 class CreateCompanyTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
     public function up()
     {
         Schema::create('companies', function (Blueprint $table) {
             $table->bigIncrements('id');
 
-            //Company
-            $table->string('regime', 120)->nullable()->default(null);
-            $table->string('token_ibpt', 255)->nullable()->default(null);
-            $table->string('csc_id', 120)->nullable()->default(null);
-            $table->string('key_csc', 255)->nullable()->default(null);
-            $table->string('certified', 255)->nullable()->default(null);
-            $table->string('password_certified', 255)->nullable()->default(null);
+            $table->string('xNome', 60);
+            $table->string('xFant', 60);
+            $table->string('IE', 14)->nullable()->default(null);
+            $table->string('IEST', 14)->nullable()->default(null);
+            $table->string('IM', 15)->nullable()->default(null);
+            $table->string('CNAE', 7)->nullable()->default(null);
+            $table->enum('CRT', [1, 2, 3])->default(1);
+            $table->string('CPFCNPJ', 14);
+            $table->enum('type', ['J', 'F'])->default('J');
 
-            //NFE
-            $table->boolean('ambient')->default(true);
-            $table->string('logo_nfe', 255)->nullable()->default(null);
-            $table->string('email_nfe', 255)->nullable()->default(null)->unique();
-            $table->string('password_email_nfe', 255)->nullable()->default(null);
-
-            //Juristic
-            $table->string('name', 255);
-            $table->string('fantasy', 255)->nullable()->default(null);
-            $table->integer('identity')->nullable()->default(null);
-            $table->string('cnpj', 14)->nullable()->default(null)->unique();
-
-            //Person
+            //Endereço
+            $table->string('xLgr', 191);
+            $table->string('nro', 60);
+            $table->string('xCpl', 60);
+            $table->string('xBairro', 60);
+            $table->string('cMun', 7);
+            $table->string('xMun', 60);
+            $table->string('UF', 2);
+            $table->string('CEP', 8);
+            $table->string('cPais', 4)->default(1058);
+            $table->string('xPais', 60)->default('Brasil');
+            $table->string('fone', 14);
             $table->boolean('active')->default(true);
-            $table->enum('type', ['J', 'P'])->default('J');
-            $table->string('cell_phone', 15)->nullable()->default(null);
-            $table->string('phone', 15)->nullable()->default(null);
-            $table->string('email', 255)->unique();
-
-            //Address
-            $table->string('neighborhood', 80);
-            $table->integer('cep');
-            $table->string('complement', 80)->nullable()->default(null);
-            $table->string('address', 255);
-            $table->string('city', 150);
-            $table->string('establishment_number', 80);
-            $table->string('state', 120);
-            $table->enum('uf', ['AC', 'AL', 'AP', 'AM', 'BA', 'CE',
-                'DF', 'ES', 'GO', 'MA', 'MT', 'MS', 'MG', 'PR', 'PB', 'PA', 'PE',
-                'PI', 'RJ', 'RN', 'RS', 'RO', 'RR', 'SC', 'SE', 'SP', 'TO']);
-
-            $table->index('active');
-            $table->index('type');
-            $table->index('ambient');
-            $table->index('cnpj');
 
             $table->timestamps();
 
         });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
     public function down()
     {
         Schema::dropIfExists('companies');
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -7,11 +7,6 @@ use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     *
-     * @return void
-     */
     public function run()
     {
         factory(User::class, 20)->create();
