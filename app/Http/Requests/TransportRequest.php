eab56ea4ec5baeedf811985a8266b1740166e081
Jesus Vieira <jesusvieiradelima@gmail.com>
2020-03-02 15:51:56 -0300

    Test funcional de transportadoras

--- app/Http/Requests/TransportRequest.php
+++ app/Http/Requests/TransportRequest.php
@@ -11,10 +11,10 @@ class TransportRequest extends BaseFormRequest
     {
         return [
             'xNome' => 'required|max:60',
-            'IE' => 'max:14',
-            'xEnder' => 'max:60',
-            'xMun' => 'max:60',
-            'UF' => 'max:2',
+            'IE' => 'nullable|max:14',
+            'xEnder' => 'required|max:60',
+            'xMun' => 'required|max:60',
+            'UF' => 'required|max:2',
             'CPFCNPJ' => [
                 'required',
                 'max:14',
